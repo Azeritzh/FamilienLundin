20a27e76a85a324b47f76997ab5ec937d8e69c8f
Armienn <nigathr@gmail.com>
2020-11-24 19:15:46 +0100

    Add games component + icon

--- apps/lundin/src/app/app.module.ts
+++ apps/lundin/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
-import { AppComponent } from "./app.component"
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { GamesComponent } from './games/games.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, GamesComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
