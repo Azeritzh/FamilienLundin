729013aef9a06cec7f1ef7fec14f2288fae5e92b
Armienn <nigathr@gmail.com>
2020-11-29 13:20:07 +0100

    Add links to various games

--- apps/lundin/src/app/games/games.component.ts
+++ apps/lundin/src/app/games/games.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from "@angular/core"
+import { Component } from "@angular/core"
+import { DomSanitizer } from "@angular/platform-browser"
 
 @Component({
 	selector: "lundin-games",
 	templateUrl: "./games.component.html",
 	styleUrls: ["./games.component.scss"]
 })
-export class GamesComponent implements OnInit {
-	constructor() { }
+export class GamesComponent {
+	gameLink: string
+	games: GameEntry[] = [
+		{ text: "Minestryger", link: "http://belrokt.github.io/Minestryger/" },
+		{ text: "Tern", link: "http://armienn.github.io/Tern" },
+		{ text: "Piong", link: "http://armienn.github.io/archive/piong" },
+		{ text: "Game of Life", link: "http://armienn.github.io/archive/game-of-life" },
+		{ text: "Bilspil", link: "http://belrokt.github.io/Bilspil/" },
+	]
 
-	ngOnInit() { }
+	constructor(private sanitizer: DomSanitizer) { }
+
+	getLink() {
+		return this.sanitizer.bypassSecurityTrustResourceUrl(this.gameLink)
+	}
+}
+
+interface GameEntry {
+	text: string
+	link: string
 }
