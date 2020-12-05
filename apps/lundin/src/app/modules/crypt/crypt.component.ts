c441815624a1f79c5afb5c2e15dd5205cea42cd9
Armienn <nigathr@gmail.com>
2020-12-05 21:37:12 +0100

    Add basic crypt functionality

--- apps/lundin/src/app/modules/crypt/crypt.component.ts
+++ apps/lundin/src/app/modules/crypt/crypt.component.ts
@@ -1,8 +1,21 @@
 import { Component } from "@angular/core"
+import { AES, enc } from "crypto-js"
 
 @Component({
 	selector: "lundin-crypt",
 	templateUrl: "./crypt.component.html",
 	styleUrls: ["./crypt.component.scss"],
 })
-export class CryptComponent { }
+export class CryptComponent {
+	key = ""
+	content = ""
+	encrypted = "U2FsdGVkX1/jGbqemIDO8kirGU7QAlxysqZb/uwqfw/DWncmuJfpyAeoGen1lzHw"
+
+	encrypt(){
+		this.encrypted = AES.encrypt(this.content, this.key).toString()
+	}
+
+	decrypt(){
+		this.content = AES.decrypt(this.encrypted, this.key).toString(enc.Utf8)
+	}
+}
--- apps/lundin/src/app/modules/crypt/crypt.module.ts
+++ apps/lundin/src/app/modules/crypt/crypt.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 import { BrowserModule } from "@angular/platform-browser"
 import { CryptComponent } from "./crypt.component"
 import { CryptService } from "./crypt.service"
@@ -8,6 +9,7 @@ import { CryptService } from "./crypt.service"
 	declarations: [CryptComponent],
 	imports: [
 		BrowserModule,
+		FormsModule,
 		HttpClientModule,
 	],
 	providers: [CryptService],
