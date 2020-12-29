e34849442673deea43ac44e410319d53a1c13071
Armienn <nigathr@gmail.com>
2020-12-29 19:17:30 +0100

    #40 - Rename folder

--- apps/lundin/src/app/modules/minestryger/minestryger.module.ts
+++ apps/lundin/src/app/modules/minestryger/minestryger.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { SharedModule } from "../../shared/shared.module"
 import { MinestrygerGameComponent } from "./minestryger-game/minestryger-game.component"
-import { MinestrygerSettingsComponent } from "./minestryger-settings.component.ts/minestryger-settings.component"
+import { MinestrygerSettingsComponent } from "./minestryger-settings/minestryger-settings.component"
 import { MinestrygerComponent } from "./minestryger.component"
 
 @NgModule({
