72fed5049bac070206389c33da4cc4ad047a3dbb
Armienn <nigathr@gmail.com>
2020-12-31 19:20:00 +0100

    #75 - Implement generateVirusActions

--- libs/virus/src/lib/virus-action.ts
+++ libs/virus/src/lib/virus-action.ts
@@ -1,3 +1,4 @@
+import { range } from "@lundin/utility"
 import { Virus } from "./virus"
 
 export class VirusAction {
@@ -17,5 +18,30 @@ export class VirusAction {
 }
 
 export function generateVirusActions(game: Virus) {
-	return [new VirusAction(game.state.currentPlayer, 0, 7, 0, 6)]
+	const width = game.config.width
+	const height = game.config.height
+	const actions = []
+	for (const i of range(0, width)) {
+		for (const j of range(0, height)) {
+			if (game.state.board.get(i, j) != 0)
+				continue
+
+			let exists = false
+			for (const n of range(Math.max(0, i - 2), Math.min(width, i + 3))) {
+				for (const m of range(Math.max(0, j - 2), Math.min(height, j + 3))) {
+					if (game.state.board.get(n, m) != game.state.currentPlayer)
+						continue
+					const action = new VirusAction(game.state.currentPlayer, n, m, i, j)
+					if (Math.abs(action.origin.x - action.destination.x) > 1 ||
+						Math.abs(action.origin.y - action.destination.y) > 1) {
+						actions.push(action)
+					} else if (!exists) {
+						actions.push(action)
+						exists = true
+					}
+				}
+			}
+		}
+	}
+	return actions
 }
