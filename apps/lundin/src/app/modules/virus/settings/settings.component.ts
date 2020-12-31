9350928431921969983176a51e782fefa13cf0a0
Armienn <nigathr@gmail.com>
2020-12-31 19:31:29 +0100

    #75 - Add virus player type select

--- apps/lundin/src/app/modules/virus/settings/settings.component.ts
+++ apps/lundin/src/app/modules/virus/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core"
+import { RandomAi } from "@lundin/age"
+import { generateVirusActions } from "@lundin/virus"
 import { VirusPlayer } from "../virus-game/virus-game.component"
 
 @Component({
@@ -25,5 +27,27 @@ export class SettingsComponent {
 	addPlayer() {
 		const newPlayerNumber = this.players.length + 1
 		this.players.push(new VirusPlayer("Spiller " + newPlayerNumber, "blue"))
+	}
+
+	typeOf(player: VirusPlayer) {
+		if (!player.ai)
+			return "human"
+		else
+			return "random"
+	}
+
+	changePlayerType(player: VirusPlayer, type: string) {
+		if (type === "human")
+			return player.ai = null
+		else if (type === "random")
+			return player.ai = new RandomAi(generateVirusActions)
+		else
+			this.removePlayer(player)
+	}
+
+	private removePlayer(player: VirusPlayer) {
+		const index = this.players.indexOf(player)
+		this.players.splice(index, 1)
+		this.playersChange.emit(this.players)
 	}
 }
