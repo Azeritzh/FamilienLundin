13419c5cec68cc722b05cd6a45cfb96d8261d637
Armienn <nigathr@gmail.com>
2020-12-31 14:21:31 +0100

    #75 - Move players to settings component

--- apps/lundin/src/app/modules/virus/settings/settings.component.ts
+++ apps/lundin/src/app/modules/virus/settings/settings.component.ts
@@ -6,6 +6,10 @@ import { Component, EventEmitter, Input, Output } from "@angular/core"
 	styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent {
+	players = [
+		{ name: "Spiller 1", color: "red", playerId: 1 },
+		{ name: "Spiller 2", color: "green", playerId: 2 },
+	]
 	@Input() boardSize = 8
 	@Output() boardSizeChange = new EventEmitter<number>()
 	@Input() fieldSize = 50
@@ -17,5 +21,15 @@ export class SettingsComponent {
 
 	toggleAdvanced() {
 		this.showAdvancedSettings = !this.showAdvancedSettings
+	}
+
+	addPlayer() {
+		this.players.push({ name: "Spiller 3", color: "blue", playerId: 3 })
+	}
+
+	restart() {
+		/*const config = new VirusConfig(this.players.length)
+		this.game = new Virus(config)
+		this.message = ""*/
 	}
 }
--- apps/lundin/src/app/modules/virus/virus.module.ts
+++ apps/lundin/src/app/modules/virus/virus.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { SharedModule } from "../../shared/shared.module"
-import { PlayersComponent } from "./players/players.component"
 import { SettingsComponent } from "./settings/settings.component"
 import { VirusGameComponent } from "./virus-game/virus-game.component"
 import { VirusComponent } from "./virus.component"
@@ -9,7 +8,6 @@ import { VirusComponent } from "./virus.component"
 @NgModule({
 	declarations: [
 		SettingsComponent,
-		PlayersComponent,
 		VirusComponent,
 		VirusGameComponent,
 	],
