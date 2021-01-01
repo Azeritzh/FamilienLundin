3049fb36c7b49124935dc66ab311181850e4565e
Armienn <nigathr@gmail.com>
2021-01-01 11:25:01 +0100

    #73 - Add period switch

--- apps/lundin/src/app/modules/minestryger/minestryger-highscores/minestryger-highscores.component.ts
+++ apps/lundin/src/app/modules/minestryger/minestryger-highscores/minestryger-highscores.component.ts
@@ -12,21 +12,32 @@ export class MinestrygerHighscoresComponent {
 	@Input() category: string
 	topScoreCategories = [
 		{ key: "beginnerFlags", title: "Begynder (med flag)" },
+		{ key: "beginnerNoFlags", title: "Begynder (uden flag)" },
 		{ key: "trainedFlags", title: "Øvet (med flag)" },
+		{ key: "trainedNoFlags", title: "Øvet (uden flag)" },
 		{ key: "expertFlags", title: "Ekspert (med flag)" },
-		{ key: "beginnerNoFlags", title: "Begynder (ingen flag)" },
-		{ key: "trainedNoFlags", title: "Øvet (ingen flag)" },
-		{ key: "expertNoFlags", title: "Ekspert (ingen flag)" },
+		{ key: "expertNoFlags", title: "Ekspert (uden flag)" },
 	]
 	topScores$: Observable<MinestrygerTopScoreSet>
 	myScores$: Observable<MinestrygerScoreSet>
+	scoresThisYear = true
 
 	constructor(minestrygerService: MinestrygerService) {
 		this.topScores$ = minestrygerService.loadTopScores()
 		this.myScores$ = minestrygerService.loadMyScores()
 	}
 
+	currentTopScoreSet$() {
+		return this.scoresThisYear
+			? this.topScores$
+			: this.topScores$
+	}
+
 	currentCategory(myScores: MinestrygerScoreSet) {
 		return myScores.categories[this.category]?.slice(0, 10) ?? []
+	}
+
+	switchPeriod() {
+		this.scoresThisYear = !this.scoresThisYear
 	}
 }
