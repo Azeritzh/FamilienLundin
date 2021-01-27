df678f35f877308beda168b5cea58121c2763c16
Armienn <nigathr@gmail.com>
2021-01-27 09:39:59 +0100

    #91 - Add LabellingDirective

---
+++ apps/lundin/src/app/shared/directives/labelling.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, HostBinding, Input } from "@angular/core"
+
+@Directive({
+	selector: "[labelling]",
+})
+export class LabellingDirective {
+	@Input() labelling: any
+
+	@HostBinding("class.raised-label") get shouldBeRaised() {
+		return this.labelling.value || document.activeElement === this.labelling
+	}
+
+	@HostBinding("class.lowered-label") get shouldNotRaised() {
+		return !this.shouldBeRaised
+	}
+}
