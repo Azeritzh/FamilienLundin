b89559c00156b8fc05ee18a4fea56136aea0ec20
Azeritzh <azhlander@outlook.com>
2021-02-16 17:18:59 +0100

    #27 - Adds styling to add and edit recipe popups

--- apps/lundin/src/app/modules/recipes/recipes.module.ts
+++ apps/lundin/src/app/modules/recipes/recipes.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { MarkdownModule } from "ngx-markdown"
 import { SharedModule } from "../../shared/shared.module"
+import { AddRecipeComponent } from "./add-recipe/add-recipe.component"
+import { EditRecipeComponent } from "./edit-recipe/edit-recipe.component"
 import { RecipesService } from "./recipes.service"
 import { RecipesComponent } from "./recipes/recipes.component"
 
 @NgModule({
 	declarations: [
 		RecipesComponent,
+		AddRecipeComponent,
+		EditRecipeComponent,
 	],
 	imports: [
 		BrowserModule,
