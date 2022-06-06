a5cca9750c750266b403c80e13196ea62ed4132e
Armienn <nigathr@gmail.com>
2022-06-06 11:30:17 +0200

    #144 Fix project configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,5 @@
-const { getJestProjects } = require("@nrwl/jest")
+import { getJestProjects } from "@nrwl/jest"
 
 export default {
-	projects: [
-		...getJestProjects(),
-		"<rootDir>/apps/lundin",
-		"<rootDir>/apps/api",
-		"<rootDir>/libs/api-interfaces",
-		"<rootDir>/libs/age",
-		"<rootDir>/libs/noughts-and-crosses",
-		"<rootDir>/libs/virus",
-		"<rootDir>/libs/utility",
-		"<rootDir>/libs/minestryger",
-		"<rootDir>/libs/meld",
-		"<rootDir>/libs/agentia",
-		"<rootDir>/libs/kingdoms",
-		"<rootDir>/libs/renderend",
-	],
+	projects: getJestProjects(),
 }
