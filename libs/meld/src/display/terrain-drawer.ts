a84e29925f151fb3e7c7ae535136fa109da38803
Armienn <nigathr@gmail.com>
2022-06-30 09:06:06 +0200

    Add temporary variant thing

--- libs/meld/src/display/terrain-drawer.ts
+++ libs/meld/src/display/terrain-drawer.ts
@@ -58,14 +58,28 @@ export class TerrainDrawer {
 	}
 
 	private drawBlockTop(blockType: string, position: Vector3) { //, variant: number) {
-		this.camera.drawVaried(blockType + "-tile", position) // , null, variant)
+		this.camera.drawVaried(blockType + "-tile", position, null, this.variantFor(position))
 	}
 
 	private drawBlockFullSide(blockType: string, position: Vector3) { //, variant: number) {
-		this.camera.drawVaried(blockType + "-wall", position) // , null, variant)
+		this.camera.drawVaried(blockType + "-wall", position, null, this.variantFor(position))
 	}
 
 	private fullWallOffset(): number {
 		return (this.config.wallDisplayHeight - 1) / this.config.wallDisplayHeight
+	}
+
+	private variantFor(position: Vector3) {
+		let x = position.x
+		let y = position.y
+		let z = position.z
+		if (x == 0)
+			x = 1234
+		if (y == 0)
+			y = 1234
+		if (z == 0)
+			z = 1234
+		const number = (x / 13 + y / 31 + z / 71) * 345
+		return Math.floor(10000 * (number - Math.floor(number)))
 	}
 }
