64b8a6f634895174dcb573af8dda20d45b9b3445
Armienn <nigathr@gmail.com>
2025-05-19 12:04:30 +0200

    Raise limit for json "uploads"

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,15 @@
 import { Logger } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app/app.module"
 import cookieParser from "cookie-parser"
+import { json } from "express"
+import { AppModule } from "./app/app.module"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const globalPrefix = "api"
 	app.setGlobalPrefix(globalPrefix)
 	app.use(cookieParser())
+	app.use(json({ limit: '5mb' }))
 	const port = process.env.PORT || 3333
 	await app.listen(port, () => {
 		Logger.log("Listening at http://localhost:" + port + "/" + globalPrefix)
